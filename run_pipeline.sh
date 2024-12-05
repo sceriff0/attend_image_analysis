d27faaf1714b7ea976618539686b4bc5dad0caa0
Chiodin Alessio <ieo7086@cn12.cluster.loc>
2024-12-05 10:21:19 +0100

    Modified crop and overlap sizes for testing

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -7,19 +7,29 @@
 #    --with-tower \
 #    --input /hpcnfs/scratch/DIMA/chiodin/repositories/attend_image_analysis/samples_23S60.csv \
 
-check=1
-while [[ $check -ne 0 ]]
-do
-    nextflow run /hpcnfs/scratch/DIMA/chiodin/repositories/attend_image_analysis/main.nf \
+#check=1
+#while [[ $check -ne 0 ]]
+#do
+#    nextflow run /hpcnfs/scratch/DIMA/chiodin/repositories/attend_image_analysis/main.nf \
+#        -with-tower \
+#        -profile singularity \
+#        -resume \
+#        --with-tower \
+#        --input /hpcnfs/scratch/DIMA/chiodin/repositories/attend_image_analysis/sample_sheet.csv \
+#        --crop_size_affine 2000\
+#        --overlap_size_affine 900 \
+#        --crop_size_diffeo 2000 \
+#        --overlap_size_diffeo 200
+#done 
+
+nextflow run /hpcnfs/scratch/DIMA/chiodin/repositories/attend_image_analysis/main.nf \
         -with-tower \
         -profile singularity \
         -resume \
         --with-tower \
         --input /hpcnfs/scratch/DIMA/chiodin/repositories/attend_image_analysis/sample_sheet.csv \
-        --crop_size_affine 2000\
-        --overlap_size_affine 900 \
-        --crop_size_diffeo 2000 \
-        --overlap_size_diffeo 200
-done 
-
+        --crop_size_affine 1500\
+        --overlap_size_affine 700 \
+        --crop_size_diffeo 1000 \
+        --overlap_size_diffeo 400
   
