15adf606279f94c3c5887d9a45d015656fc1728a
Chiodin Alessio <ieo7086@cn12.cluster.loc>
2025-03-03 11:16:07 +0100

    Fixed get_max_axis_value

--- bin/get_padding.py
+++ bin/get_padding.py
@@ -12,18 +12,18 @@ logging_config.setup_logging()
 logger = logging.getLogger(__name__)
 
 def get_max_axis_value(files):
-    format = os.path.basename(files[0]).split('.')[1]
-    if format == 'h5':
-        shapes = [(get_image_file_shape(file, format=format)[0], get_image_file_shape(file, format=format)[1]) for file in files]
-        logger.debug(f'Shapes: {shapes}')
-    elif format == 'nd2':
-        shapes = [get_image_file_shape(file, format=format) for file in files]
-        logger.debug(f'Shapes: {shapes}')
-    else:
-        # Handle unsupported formats
-        logger.error(f"Unsupported file format: {format}")
-        raise ValueError(f"Unsupported file format: {format}")
-
+    formats = [os.path.basename(file).split('.')[1] for file in files]
+    #if format == 'h5':
+    #    shapes = [(get_image_file_shape(file, format=format)[0], get_image_file_shape(file, format=format)[1]) for file, format in zip(files, formats)]
+    #    logger.debug(f'Shapes: {shapes}')
+    #elif format == 'nd2':
+    #    shapes = [get_image_file_shape(file, format=format) for file in files]
+    #    logger.debug(f'Shapes: {shapes}')
+    #else:
+    #    # Handle unsupported formats
+    #    logger.error(f"Unsupported file format: {format}")
+    #    raise ValueError(f"Unsupported file format: {format}")
+    shapes = [(get_image_file_shape(file, format=format)[0], get_image_file_shape(file, format=format)[1]) for file, format in zip(files, formats)]
     max_shape = tuple(map(max, zip(*shapes)))
 
     return max_shape
