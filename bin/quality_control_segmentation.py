bfbe5e279f51c13c0dba25bd897c0a3d82e2e9e1
Chiodin Alessio <ieo7086@cn13.cluster.loc>
2025-04-03 10:34:49 +0200

    Added output image downsampling

--- bin/quality_control_segmentation.py
+++ bin/quality_control_segmentation.py
@@ -8,12 +8,28 @@ import argparse
 import tifffile as tiff
 import logging
 from cellpose.utils import masks_to_outlines
+from skimage.transform import rescale
 from utils import logging_config
 
 # Set up logging configuration
 logging_config.setup_logging()
 logger = logging.getLogger(__name__)
 
+def normalize_image(image):
+    """Normalize each channel of the image independently to [0, 255] uint8."""
+    min_val = image.min(axis=(1, 2), keepdims=True)
+    max_val = image.max(axis=(1, 2), keepdims=True)
+    scaled_image = (image - min_val) / (max_val - min_val) * 255
+    return scaled_image.astype(np.uint8)
+
+def rescale_to_uint8(image):
+    # Rescale downsampled image to uint8
+    min_val = image.min(axis=(1, 2), keepdims=True)
+    max_val = image.max(axis=(1, 2), keepdims=True)
+    image = (image - min_val) / (max_val - min_val) * 255
+    image = image.astype(np.uint8)
+
+    return image
 
 def _parse_args():
     """Parse command-line arguments."""
@@ -64,6 +80,17 @@ if __name__ == '__main__':
     outlines = np.array(outlines, dtype = 'float32')
 
     output_array = np.stack((dapi, mask, outlines), axis=0).astype('float32')
+
+    # Normalize each channel independently
+    output_array = normalize_image(output_array)
+
+    # Downsample each channel separately
+    output_array = np.array([
+        rescale(channel, scale=0.25, anti_aliasing=True) for channel in output_array
+    ])
+
+    output_array = rescale_to_uint8(output_array)
+    
 
     tiff.imwrite(f"QC_segmentation_{dapi_image}", output_array)
 
